94d3aeaff88003542752aaf54db61e2eb7c8d585
jpslat1026 <jpslat1026@gmail.com>
2019-05-07 08:15:23 -0400

    MinWiki/MinWiki/settings: updated

--- MinWiki/MinWiki/settings.py
+++ MinWiki/MinWiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ga@b)ojzjh9yo5_p5b-yvm(&-=r)57fcbvp%xc#ee0u11o-dat'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wiki.apps.WikiConfig'
 ]
 
 MIDDLEWARE = [
